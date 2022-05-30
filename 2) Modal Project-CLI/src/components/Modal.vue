3d6ab60dd835c8ec2d0570cb4beea41ceb1385b6
Yun Chung <yun.chung@mac.com>
2022-05-30 09:35:30 +0900

    created styled modal component

---
+++ 2) Modal Project-CLI/src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 96px auto;
+  background: white;
+  border-radius: 8px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
