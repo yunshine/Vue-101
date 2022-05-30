b36dd36d2f7e0ae17f8b95d5b4b35ab03044fe8f
Yun Chung <yun.chung@mac.com>
2022-05-30 09:35:58 +0900

    imported modal component to app component

--- 2) Modal Project-CLI/src/App.vue
+++ 2) Modal Project-CLI/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+};
 </script>
 
 <style>
