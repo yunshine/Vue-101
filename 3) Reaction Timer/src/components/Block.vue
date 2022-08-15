5245770af9779d74d1b5f7e4286ee0bebf2fdc92
Yun Chung <yunseok_chung@degica.com>
2022-08-15 14:41:46 +0900

    reaction timer game has timer start and stop functionality

--- 3) Reaction Timer/src/components/Block.vue
+++ 3) Reaction Timer/src/components/Block.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class='block' v-if='showBlock'>click me</div>
+  <div class='block' v-if='showBlock' @click='stopTimer'>click me</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      showBlock: false
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
     }
   },
   props: ['delay'], 
@@ -14,13 +16,25 @@ export default {
   mounted() {
     console.log('component mounted...')
     console.log(`delay = ${this.delay}`)
-    setTimeout(() => { this.showBlock = true }, this.delay)
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+      }, this.delay)
   },
   updated() {
     console.log('component updated...')
   },
   unmounted() {
     console.log('unmounted...') // in this app, this component never actually unmounts; after all, there are no other pages to go to...
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => { this.reactionTime += 10 }, 10) // the 10 is 10 milliseconds...
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(`reaction time: ${this.reactionTime}`)
+    }
   }
 }
 </script>
