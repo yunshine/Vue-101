162bd7ac58df978b54bf2219d193a73139c58875
Yun Chung <yunseok_chung@degica.com>
2022-08-15 14:20:17 +0900

    code and notes on life-cycle hooks

--- 3) Reaction Timer/src/components/Block.vue
+++ 3) Reaction Timer/src/components/Block.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class='block'>click me</div>
+  <div class='block' v-if='showBlock'>click me</div>
 </template>
 
 <script>
 export default {
-  props: ['delay']
+  data() {
+    return {
+      showBlock: false
+    }
+  },
+  props: ['delay'], 
+  // life-cycle hooks...
+  mounted() {
+    console.log('component mounted...')
+    console.log(`delay = ${this.delay}`)
+    setTimeout(() => { this.showBlock = true }, this.delay)
+  },
+  updated() {
+    console.log('component updated...')
+  },
+  unmounted() {
+    console.log('unmounted...') // in this app, this component never actually unmounts; after all, there are no other pages to go to...
+  }
 }
 </script>
 
