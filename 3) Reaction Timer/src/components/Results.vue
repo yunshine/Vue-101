cccfae3b7ecd01bda6d2bac497595224809ff8d9
Yun Chung <yunseok_chung@degica.com>
2022-08-20 11:03:06 +0900

    used mounted hook to display a comment for reaction time score

--- 3) Reaction Timer/src/components/Results.vue
+++ 3) Reaction Timer/src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
   <h3>Your Reaction Time: {{ score }} ms</h3>
+  <h2 class='rank'>{{ rank }}</h2>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers!!!'
+    } else if (this.score < 350) {
+      this.rank = 'Rapid Reflexes!!!'
+    } else {
+      this.rank = "You're a snail..."
+    }
+  }
 }
 </script>
 
