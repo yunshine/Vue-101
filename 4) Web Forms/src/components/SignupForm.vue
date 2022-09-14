413d5d5ab1f47a1842fd8d16fcb389bdbd132516
Yun Chung <yunseok_chung@degica.com>
2022-09-14 17:24:54 +0900

    signup form css

--- 4) Web Forms/src/components/SignupForm.vue
+++ 4) Web Forms/src/components/SignupForm.vue
@@ -12,5 +12,30 @@ export default {
 </script>
 
 <style>
-
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 8px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
 </style>
