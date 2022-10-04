3e608c102f031371313383c40ffed4e8d5ea4ead
Yun Chung <yunseok_chung@degica.com>
2022-10-04 12:19:35 +0900

    deleted unnecessary vue template files

--- 5) Vue Router Basics/src/views/HomeView.vue
+++ 5) Vue Router Basics/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit possimus eum pariatur earum repellat laboriosam, porro alias eius facilis tenetur aliquam cumque tempora provident quas necessitatibus assumenda? Sed, neque tempora soluta explicabo cum magnam earum officiis reprehenderit illum deleniti laborum nisi harum id alias incidunt enim debitis. Rem ab, ullam hic quas maxime cumque minima nihil accusantium deleniti, eius harum aliquid neque nam temporibus earum nulla doloremque nemo ea saepe illum dolorum. Sequi dicta numquam impedit, laborum explicabo qui itaque est suscipit asperiores, doloribus dolor quas accusantium? Iusto similique consectetur saepe dolores provident laudantium eos, voluptatum temporibus? Exercitationem, blanditiis!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit possimus eum pariatur earum repellat laboriosam, porro alias eius facilis tenetur aliquam cumque tempora provident quas necessitatibus assumenda? Sed, neque tempora soluta explicabo cum magnam earum officiis reprehenderit illum deleniti laborum nisi harum id alias incidunt enim debitis. Rem ab, ullam hic quas maxime cumque minima nihil accusantium deleniti, eius harum aliquid neque nam temporibus earum nulla doloremque nemo ea saepe illum dolorum. Sequi dicta numquam impedit, laborum explicabo qui itaque est suscipit asperiores, doloribus dolor quas accusantium? Iusto similique consectetur saepe dolores provident laudantium eos, voluptatum temporibus? Exercitationem, blanditiis!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
