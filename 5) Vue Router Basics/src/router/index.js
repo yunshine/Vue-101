d63c778199d6600396ad0bae7a0119d1c2aff160
Yun Chung <yunseok_chung@degica.com>
2022-10-07 17:08:18 +0900

    JobDetails route

--- 5) Vue Router Basics/src/router/index.js
+++ 5) Vue Router Basics/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     path: '/jobs',
     name: 'jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails
   }
 ]
 
