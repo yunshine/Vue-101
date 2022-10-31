1fd55cf75ec75cd8d2c33427679f5060fa473019
Yun Chung <yunseok_chung@degica.com>
2022-10-31 18:01:53 +0900

    jobs fetched from api when component mounts

--- 5) Vue Router Basics/src/views/jobs/Jobs.vue
+++ 5) Vue Router Basics/src/views/jobs/Jobs.vue
@@ -12,11 +12,19 @@ export default {
   data() {
     return {
       jobs: [
-        { title: 'UX Designer', id: 309, details: 'lorem' },
-        { title: 'Web Developer', id: 718, details: 'lorem' },
-        { title: 'Vue Developer', id: 122, details: 'lorem' }
+        // { title: 'UX Designer', id: 309, details: 'lorem' },
+        // { title: 'Web Developer', id: 718, details: 'lorem' },
+        // { title: 'Vue Developer', id: 122, details: 'lorem' }
+
       ]
     }
+  },
+  // a good place for fetching data is the mounted lifecycle hook:
+  mounted() {
+    fetch('http://localhost:3000/jobs') // returns a promise
+      .then(res => res.json()) // this also returns a promise
+      .then(data => this.jobs = data) // data is an array of job hashes, so we're setting this.jobs to be the array we get back
+      .catch(err => console.log(err.message)) // in case something goes wrong
   }
 }
 </script>
